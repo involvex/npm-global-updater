666e01b65a9f473c35b82ff2b2f69450ae1c2734
InvolveX <lukaswill97@gmail.com>
2025-12-19 19:23:39 +0100

    updated

--- bin/npm-updater.js
+++ bin/npm-updater.js
@@ -2263,7 +2263,7 @@ var package_default;
 var init_package = __esm(() => {
   package_default = {
     name: "@involvex/npm-global-updater",
-    version: "0.1.29",
+    version: "0.1.30",
     description: "global npm package updater",
     license: "MIT",
     author: "involvex",
