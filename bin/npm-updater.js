8d7eb3362e0e7e5af9d1e040ae4bb1b2eed68828
InvolveX <lukaswill97@gmail.com>
2025-12-19 22:31:17 +0100

    ci(workflow): add version bumping to release pipeline
    Automatically bump patch version using 'bun pm version patch' in the release workflow to ensure consistent versioning during automated releases.

--- bin/npm-updater.js
+++ bin/npm-updater.js
@@ -2284,7 +2284,7 @@ var package_default;
 var init_package = __esm(() => {
   package_default = {
     name: "@involvex/npm-global-updater",
-    version: "0.1.34",
+    version: "0.1.35",
     description: "global npm package updater",
     license: "MIT",
     author: "involvex",
