d0c9c8b5345d0dcd9b2088113e82664e6871aceb
InvolveX <lukaswill97@gmail.com>
2025-12-19 13:01:59 +0100

    chore(updater): bump version to 0.1.9

--- bin/npm-updater.js
+++ bin/npm-updater.js
@@ -1805,7 +1805,7 @@ var package_default;
 var init_package = __esm(() => {
   package_default = {
     name: "@involvex/npm-global-updater",
-    version: "0.1.8",
+    version: "0.1.9",
     description: "global npm package updater",
     license: "MIT",
     author: "involvex",
