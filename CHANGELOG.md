cbd8e5034c4700c413172355611194e19df65a08
InvolveX <lukaswill97@gmail.com>
2025-12-19 07:32:33 +0100

    ## v0.0.11...main
    [compare changes](https://github.com/involvex/npm-global-updater/compare/v0.0.11...main)
    
    ## v0.0.11...v0.0.11
    
    [compare changes](https://github.com/involvex/npm-global-updater/compare/v0.0.11...v0.0.11)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## v0.0.11...main
+
+[compare changes](https://github.com/involvex/npm-global-updater/compare/v0.0.11...main)
+
 ## v0.0.11...v0.0.11
 
 [compare changes](https://github.com/involvex/npm-global-updater/compare/v0.0.11...v0.0.11)
--- scripts/release.ts
+++ scripts/release.ts
@@ -3,16 +3,16 @@ import { exec } from "child_process";
 export async function runrelease() {
   console.log("Starting Release script...");
   console.log("Building project...");
-  exec("bun run build");
+  await exec("bun run build");
   console.log("Project built successfully.");
   console.log("Generate Changelog");
-  exec("bun run changelog");
+  await exec("bun run changelog");
   console.log("Commiting changes...");
-  exec("git add .");
-  exec("git commit -F Changelog.md");
+  await exec("git add .");
+  await exec("git commit -F Changelog.md");
   console.log("Increasing Version");
-  exec("npm version patch");
+  await exec("npm version patch");
   console.log("Pushing changes...");
-  exec("git push --all");
+  await exec("git push --all");
 }
 runrelease();
